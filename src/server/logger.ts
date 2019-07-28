ac36d6fe9502542d37bd86574b792c746c241f3a
CodeLionX <sebastian.schmidl@t-online.de>
2019-07-28 16:03:09 +0200

    improve logging file

--- src/server/logger.ts
+++ src/server/logger.ts
@@ -1,19 +1,25 @@
-import winston from "winston";
+import winston, {Logger} from "winston";
 
 const environment: string = process.env.NODE_ENV;
 
-export const logger = winston.createLogger({
-    level: "info",
-    format: winston.format.json(),
-    defaultMeta: { service: "user-service" },
-    transports: [
-        new winston.transports.File({ filename: "server.log" })
-    ]
-});
+export function getLogger(name: string): Logger {
+    const log = winston.createLogger({
+        level: "info",
+        format: winston.format.json(),
+        defaultMeta: { service: name },
+        transports: [
+            new winston.transports.File({ filename: "server.log" })
+        ]
+    });
 
-if (environment !== "production") {
-    logger.add(new winston.transports.Console({
-        // simple format: `${info.level}: ${info.message} JSON.stringify({ ...rest })`
-        format: winston.format.simple()
-    }));
+    if (environment !== "production") {
+        log.add(new winston.transports.Console({
+            // simple format: `${info.level}: ${info.message} JSON.stringify({ ...rest })`
+            format: winston.format.simple()
+        }));
+    }
+    return log;
 }
+
+// default logger
+export const logger: Logger = getLogger("server");
